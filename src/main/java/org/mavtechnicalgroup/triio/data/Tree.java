478bcbd904b99b939fa8dcdc9bb6a7c183dca76f
Sarcross <antzyee@yahoo.com>
2015-01-30 15:39:46 -0800

    Added method signatures for Tree.java

--- src/main/java/org/mavtechnicalgroup/triio/data/Tree.java
+++ src/main/java/org/mavtechnicalgroup/triio/data/Tree.java
@@ -1,13 +1,49 @@
 package org.mavtechnicalgroup.triio.data;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
-public class Tree {
+/**
+ * Common info about a "Tree" to be the super class
+ * for TraditionalTree and FraternityTree data containers.
+ * Fraternity Tree needs to implement further search methods
+ * - Chapter
+ * - Line
+ * @author Anthony Haddox
+ * @author Michelle Chuong
+ *
+ */
+public abstract class Tree {
 	private String UID;
+	private Map<String, FamilyMember> nodeMap;
 	
 	public Tree()	{
 		UID = generateUID();
+		nodeMap = new HashMap<String, FamilyMember>();
 	}
+	
+	//Adding to tree
+	//public void add(String fn, String ln, String mn, String g, Date d)
+	
+	//Adding to tree
+	//public void add(String fn, String ln, String mn, String g)
+	
+	//Adding to tree
+	//public void add(String fn, String ln, String g)
+	
+	//Adding to tree
+	//public void add(FamilyMember source)
+	
+	//Remove from tree
+	//public void remove(FamilyMember source)
+	
+	//Search by First Name & Last Name
+	//public FamilyMember find(String first, String last)
+	
+	//Search by UID
+	//public FamilyMember find(String uid);
 	
 	private String generateUID()	{
 		Random rnd = new Random();
@@ -20,4 +56,5 @@ public class Tree {
 	}
 	
 	public String getUID()	{	return UID;	}
+	public Map<String, FamilyMember> getNodeMap()	{	return nodeMap;	}
 }
