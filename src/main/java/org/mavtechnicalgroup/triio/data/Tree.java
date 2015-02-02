b7d11cf4836d7ed4645952e50004ef1fb7b2c6cd
mchuong <mchuong@csupomona.edu>
2015-02-02 12:07:01 -0800

    filled in methods
    adding to tree, remove from tree, search by UID

--- src/main/java/org/mavtechnicalgroup/triio/data/Tree.java
+++ src/main/java/org/mavtechnicalgroup/triio/data/Tree.java
@@ -25,25 +25,40 @@ public abstract class Tree {
 	}
 	
 	//Adding to tree
-	//public void add(String fn, String ln, String mn, String g, Date d)
+    public void add(String fn, String ln, String mn, String g, Date d) {
+        FamilyMember member = new FamilyMember(fn, ln, mn, g, d);
+        nodeMap.put(member.generateUID, member);
+    }
 	
 	//Adding to tree
-	//public void add(String fn, String ln, String mn, String g)
+    public void add(String fn, String ln, String mn, String g) {
+        FamilyMember member = new FamilyMember(fn, ln, mn, g);
+        nodeMap.put(member.generateUID), member);
+    }
 	
 	//Adding to tree
-	//public void add(String fn, String ln, String g)
+    public void add(String fn, String ln, String g) {
+        FamilyMember member = new FamilyMember(fn, ln, g);
+        nodeMap.put(member.generateUID, member);
+    }
 	
 	//Adding to tree
-	//public void add(FamilyMember source)
+    public void add(FamilyMember source) {
+        nodeMap.put(source.generateUID, source);
+    }
 	
 	//Remove from tree
-	//public void remove(FamilyMember source)
+    public void remove(FamilyMember source) {
+        nodeMap.remove(source.getUID);
+    }
 	
 	//Search by First Name & Last Name
 	//public FamilyMember find(String first, String last)
 	
 	//Search by UID
-	//public FamilyMember find(String uid);
+    public FamilyMember find(String uid) {
+        return map.get(uid);
+    }
 	
 	private String generateUID()	{
 		Random rnd = new Random();
