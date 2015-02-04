3cd900a4649851b1f4f4a2c1d0bbe843942e5b6e
mchuong <mchuong@users.noreply.github.com>
2015-02-04 12:23:21 -0800

    Create TraditionalTree.java
    create Traditional Tree extends Tree

---
+++ src/main/java/org/mavtechnicalgroup/triio/data/TraditionalTree.java
@@ -0,0 +1,14 @@
+package org.mavtechnicalgroup.triio.data;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+public class TraditionalTree extends Tree {
+  
+  public TraditionalTree() {
+  
+  }
+  
+}
