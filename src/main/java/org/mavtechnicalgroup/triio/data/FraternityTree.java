0c14eecd50708c78549267eaf11b23224f2193ce
mchuong <mchuong@users.noreply.github.com>
2015-02-11 13:40:44 -0800

    Update FraternityTree.java
    find methods

--- src/main/java/org/mavtechnicalgroup/triio/data/FraternityTree.java
+++ src/main/java/org/mavtechnicalgroup/triio/data/FraternityTree.java
@@ -11,14 +11,28 @@ public class FraternityTree extends Tree {
   
   }
   
-  //Search by Family
-  //public FraternityFamilyMember find(String fam) {
+  //Search by Chapter
+  public FraternityFamilyMember findChapter(String chapter) {
+      for (String key : nodeMap.keySet()) {
+        if (nodeMap.get(key).getChapter().equals(chapter.toUpperCase()))
+          return nodeMap.get(key);
+        }
+  }
   
-  //}
+  //Search by Family
+  public FraternityFamilyMember findFamily(String fam) {
+      for (String key : nodeMap.keySet()) {
+        if (nodeMap.get(key).getFamily().equals(fam.toUpperCase()))
+          return nodeMap.get(key);
+      }
+  }
   
   //Search by Line
-  //public FraternityFamilyMember find(String line) {
-  
-  //}
+  public FraternityFamilyMember findLine(String line) {
+      for (String key : nodeMap.keySet()) {
+        if (nodeMap.get(key).getLine().equals(line.toUpperCase()))
+          return nodeMap.get(key);
+      }
+  }
   
 }
