f634c47724bae194e03c6d43695ed9b897010e73
mchuong <mchuong@csupomona.edu>
2015-02-16 22:35:48 -0800

    resources folder

--- src/main/java/org/mavtechnicalgroup/triio/web/WebController.java
+++ src/main/java/org/mavtechnicalgroup/triio/web/WebController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.mavtechnicalgroup.triio.App;
+import org.mavtechnicalgroup.triio.data.TraditionalFamilyMember;
+
 @RestController
 public class WebController {
 	
